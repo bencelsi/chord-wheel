56f519fa962f45133177aefc485a2ca373a6ff37
Ben Celsi <benjamin.d.celsi@gmail.com>
2024-04-21 21:30:48 -0700

    sc

---
+++ MusicCompendium.java
@@ -0,0 +1,19 @@
+
+public class MusicCompendium { 
+
+	public static void main(String[] args) {
+		System.out.println("Start");
+	}
+
+}
+
+
+
+/* What are some musical objects?
+
+SessionQuestionNode
+
+SessionOutcomes
+
+ProcessPhases
+*/
